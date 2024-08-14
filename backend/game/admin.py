73cb45d6d94957ba26578fdbae922ff5f4406cb8
triguiahmed <trigui.ahmed001@gmail.com>
2024-08-14 18:37:14 +0000

    add front (default settings)

--- backend/game/admin.py
+++ backend/game/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('id', 'player1', 'player2', 'current_turn', 'status')
+    list_filter = ('status', 'player1', 'player2')
+    search_fields = ('player1__username', 'player2__username', 'status')
+
+@admin.register(DiceRoll)
+class DiceRollAdmin(admin.ModelAdmin):
+    list_display = ('id', 'game', 'roll_value', 'player')
+    list_filter = ('game', 'player')
+    search_fields = ('game__id', 'player__username')
